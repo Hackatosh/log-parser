0d9598d5ab2528677b40eb766ccdf5660e948d20
Hackatosh <edouard.benauw@student.ecp.fr>
2021-10-09 22:08:25 +0200

    Add timezone for moment

--- src/helpers/format-timestamp.ts
+++ src/helpers/format-timestamp.ts
@@ -1,3 +1,7 @@
-import moment from 'moment';
+import moment from 'moment-timezone';
+import 'moment/locale/fr';
+
+moment.locale('fr');
+moment.tz.setDefault('Europe/Paris');
 
 export const formatTimestamp = (timestamp: number): string => moment(timestamp * 1000).format('DD-MM-YYYY HH:mm:ss');
