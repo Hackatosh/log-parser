7fe0c4e5c76359feee756e1a21d4c31fceecaca5
Hackatosh <edouard.benauw@student.ecp.fr>
2021-10-09 22:22:08 +0200

    Add configurable rps threshold

--- src/helpers/parse-args.ts
+++ src/helpers/parse-args.ts
@@ -3,6 +3,7 @@ import { ParsedArgs } from 'minimist';
 import { LogParserArgs } from '../typings/log-parser-args';
 
 export const DEFAULT_FILE_PATH = `${process.cwd()}/resources/sample_csv.txt`;
+export const DEFAULT_ALERT_RPS_THRESHOLD = 10;
 
 export const parseArgs = (args: ParsedArgs): LogParserArgs => {
   let logFilePath: string;
@@ -13,5 +14,18 @@ export const parseArgs = (args: ParsedArgs): LogParserArgs => {
     logFilePath = args.logFilePath.toString();
     console.log(`Using provided logFilePath : ${logFilePath}`);
   }
-  return { logFilePath };
+
+  let alertRpsThreshold: number;
+  if (!args.alertRpsThreshold) {
+    alertRpsThreshold = DEFAULT_ALERT_RPS_THRESHOLD;
+    console.log(`alertRpsThreshold. Using default alertRpsThreshold : ${alertRpsThreshold}`);
+  } else {
+    alertRpsThreshold = Number(args.alertRpsThreshold);
+    if (Number.isNaN(alertRpsThreshold)) {
+      throw new Error(`Provided alertRpsThreshold "${args.alertRpsThreshold}" is not a number`);
+    }
+    console.log(`Using provided alertRpsThreshold : ${alertRpsThreshold}`);
+  }
+
+  return { logFilePath, alertRpsThreshold };
 };
