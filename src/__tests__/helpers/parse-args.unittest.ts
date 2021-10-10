6e1a60d1429a8831a46374f3a3b7069b7d011a10
Edouard Benauw <edouard.benauw@student.ecp.fr>
2021-10-10 12:01:31 +0200

    Improve args parsing (#10)

--- src/__tests__/helpers/parse-args.unittest.ts
+++ src/__tests__/helpers/parse-args.unittest.ts
@@ -7,6 +7,10 @@ describe('Parse args', () => {
     expect(parseArgs({ _: [], alertRpsThreshold: 3 })).toEqual({ logFilePath: DEFAULT_FILE_PATH, alertRpsThreshold: 3 });
     expect(() => parseArgs({ _: [], alertRpsThreshold: 'unexpected-string' }))
       .toThrow(new Error('Provided alertRpsThreshold "unexpected-string" is not a number'));
+    expect(() => parseArgs({ _: [], alertRpsThreshold: -1 }))
+      .toThrow(new Error('Provided alertRpsThreshold "-1" is inferior or equal to 0, it must be positive'));
+    expect(() => parseArgs({ _: [], alertRpsThreshold: 0 }))
+      .toThrow(new Error('Provided alertRpsThreshold "0" is inferior or equal to 0, it must be positive'));
     expect(parseArgs({ _: [], logFilePath: 'test.csv', alertRpsThreshold: 3 })).toEqual({ logFilePath: 'test.csv', alertRpsThreshold: 3 });
   });
 });
